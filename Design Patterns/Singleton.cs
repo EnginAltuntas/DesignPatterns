e0764b344048952169aaf0b24e47bddf3140c5eb
Engin Altuntaş <61109013+EnginAltuntas@users.noreply.github.com>
2020-12-13 16:57:57 +0300

    1.0
    Singleton

---
+++ Design Patterns/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns
+{
+    class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private static int sayi = 0;
+        public Singleton()
+        {
+            Console.WriteLine("Constructor girildi");
+        }
+        public static Singleton getSıngleton()
+        {
+            if(singleton==null)
+            {
+                lock (singleton) //Multi Thread
+                {
+                    if (singleton == null)
+                    {
+                        singleton = new Singleton();
+                    }
+                }
+            }
+
+            sayi++;
+            Console.WriteLine(sayi);
+            return singleton;
+        }
+
+    }
+}
